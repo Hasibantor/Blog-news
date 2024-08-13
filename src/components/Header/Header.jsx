f8d19450385dddb43e9ab4cbafa3dd6e8b9e98b5
Hasibantor <hasib54069@gmail.com>
2024-08-13 21:35:19 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div className="flex justify-between p-12">
+                  <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+                  <img className="w-10 rounded-full" src="https://i.ibb.co/DfNTJdS/profile.png" alt="" />
+
+        </div>
+    );
+};
+
+export default Header;
